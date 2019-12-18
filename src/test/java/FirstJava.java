b990a80a120d1ce1735eec24fee64c492ac20dd1
sonu.m <sonu.mittal@afourtech.com>
2019-12-18 17:33:37 +0530

    config.yml

--- src/test/java/FirstJava.java
+++ src/test/java/FirstJava.java
@@ -2,5 +2,7 @@ public class FirstJava {
 
     public static void main(String[] args) {
         System.out.println("please test it on circleci");
+        String a=null;
+        a.trim();
     }
 }
